182026f70da833bcc7bb0edfbb7e24b5141eb8b5
ultra_pitbull <rosolovskiy2005@gmail.com>
2022-12-14 14:17:12 +0200

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,8 +3,9 @@ console.log("Hello world");
 window.TrelloPowerUp.initialize({
     "card-badges": (t, ops) => {
         t.card("name")
-            .then((card) => {
-                console.log(card);
+            .get("name")
+            .then((cardName) => {
+                console.log(cardName);
             })
             .catch((err) => {
                 console.log(err);
